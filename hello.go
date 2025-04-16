bf65813b248bd5c5343572ba7b0733f0def87540
Dannyblazer <nnajidanny004@gmail.com>
2025-04-16 17:59:16 +0100

    list struct

--- hello.go
+++ hello.go
@@ -3,14 +3,32 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 )
 
-func main() {
-	var pi64 = math.Pi
-	var pi32 float32 = math.Pi
+type Coin struct {
+	name  string
+	value float64
+}
 
-	fmt.Printf("%f\n", pi64)
-	fmt.Printf("%.2f\n", pi64)
-	fmt.Printf("%5.2f\n", pi64)
-	fmt.Println(pi32)
+func main() {
+
+	bank := 0.0
+	money := []Coin{
+		{name: "nickel", value: 0.05},
+		{name: "dime", value: 0.10},
+		{name: "quarter", value: 0.25},
+	}
+	check := math.Abs(20.0-20.05) > 0.0001
+	fmt.Println(check)
+
+	for bank < 20.0 {
+		money := money[rand.Intn(len(money))]
+		bank += money.value
+		//time.Sleep(1 * time.Second)
+		fmt.Printf("You Added %s to your bank with new balance %5.2f\n", money.name, bank)
+		fmt.Println(check)
+	}
+	fmt.Printf("You have %5.2f in you bank\n", bank)
+
 }
