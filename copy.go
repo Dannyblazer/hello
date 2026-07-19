3be3258a5ca7577b21b37230f866966b10a3af0e
Dannyblazer <nnajidanny00@gmail.com>
2026-07-19 13:09:27 +0100

    leet exercise

--- copy.go
+++ copy.go
@@ -1,40 +1,56 @@
-package mainz
+package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 func main() {
-	orders := make(chan string)
-	var wg sync.WaitGroup
-
-	// go func() {
-	// 	for i := 0; i <= 5; i++ {
-	// 		order := fmt.Sprintf("Order %d", i)
-	// 		orders <- order
-	// 		fmt.Println("Order placed")
-	// 	}
-	// 	close(orders)
-	// }()
-
-	for i := 0; i <= 5; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			order := fmt.Sprintf("Order %d", i)
-			orders <- order
-			fmt.Printf("%s placed\n", order)
-		}(i)
-	}
-
-	go func() {
-		wg.Wait()
-		close(orders)
-
-	}()
-
-	for order := range orders {
-		fmt.Println("Delivered ", order)
-	}
+	fmt.Println("Valid Parenthesis Checker Started...")
+	//testing := "((){})"
+	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	val := 2
+	//fmt.Println(checker(testing))
+	fmt.Println(removeElement(nums, val))
 }
+
+func removeElement(nums []int, val int) (int, []int) {
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			fmt.Println("kth value: ", nums[k])
+			fmt.Println("ith value: ", nums[i])
+			fmt.Println("Spaced")
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k, nums[:k]
+}
+
+// func checker(s string) bool {
+// 	if len(s)%2 != 0 {
+// 		return false
+// 	}
+
+// 	pairs := map[rune]rune{
+// 		')': '(',
+// 		'}': '{',
+// 		']': '[',
+// 	}
+
+// 	stack := []rune{}
+
+// 	for _, ch := range s {
+// 		switch ch {
+// 		case '(', '{', '[':
+// 			stack = append(stack, ch) // ["(", "("] || ["(", "{"]
+// 		case ')', '}', ']':
+// 			if len(stack) == 0 || stack[len(stack)-1] != pairs[ch] {
+// 				return false
+// 			}
+// 			stack = stack[:len(stack)-1]
+// 		}
+// 	}
+
+// 	return len(stack) == 0
+// }
